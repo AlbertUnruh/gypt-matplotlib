76134d94766975ae51f841233514ddf583e3e4ab
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2025-03-20 18:58:29 +0100

    fix tests (image comparison)

--- tests/test_context_managers.py
+++ tests/test_context_managers.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 # third party
 import matplotlib.pyplot as plt
+from PIL import Image, ImageChops
 
 # first party
 from gypt_matplotlib import context_managers
@@ -19,4 +20,7 @@ def test_au_plot(img_au_plot: Path, out_png_path: Path):
     with context_managers.au_plot(), context_managers.auto_save(out_png_path):
         plt.title(r"plot with $\text{a.u.}$")
     assert img_au_plot != out_png_path, "Output and control are the same!"
-    assert img_au_plot.read_bytes() == out_png_path.read_bytes(), f"`{img_au_plot}` and `{out_png_path}` are different!"
+
+    img_diff = ImageChops.difference(Image.open(out_png_path), Image.open(img_au_plot))
+    bytes_diff = img_diff.tobytes().replace(b"\x00", b"")
+    assert len(bytes_diff) == 0, f"`{img_au_plot}` and `{out_png_path}` are different!"
